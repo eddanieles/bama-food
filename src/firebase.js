086c1ee8c5fd2ec81cdae507e77cf4757ea92551
Ed Danieles <eddanieles@gmail.com>
2021-05-10 21:09:38 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import { firebase } from '@firebase/app'
+import '@firebase/auth'
+import '@firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCQBccYqCJKX4pWNzaOpAUX_ox5bXrjPLY",
+    authDomain: "bama-food-thing.firebaseapp.com",
+    projectId: "bama-food-thing",
+    storageBucket: "bama-food-thing.appspot.com",
+    messagingSenderId: "463193229572",
+    appId: "1:463193229572:web:2b5e50203b74f9fcd72851",
+    measurementId: "G-DJ3L3T9986"
+  };
+firebase.initializeApp(firebaseConfig)
+
+// utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+// collection references
+const usersCollection = db.collection('users')
+
+// export utils/refs
+export {
+    db,
+    auth,
+    usersCollection
+}
