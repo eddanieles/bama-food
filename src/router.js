4fd42eb24865cda1383f8e245d4679a132581bf3
Ed Danieles <eddanieles@gmail.com>
2021-05-19 02:32:35 -0400

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,50 +1,60 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Layout from './layouts/Layout'
+import Home from './views/Home'
+import Login from './views/Login'
+import Dashboard from './views/Dashboard'
+import Friends from './views/Friends'
+import Matched from './views/Matched'
+import Profile from './views/Profile'
+
 Vue.use(Router)
+
+const routes = [
+    {
+        path: '/',
+        redirect: 'home',
+        component: Layout,
+        children: [
+            // Components
+            {
+                name: 'Home',
+                path: 'home',
+                component: Home,
+            },
+            {
+                name: 'Login',
+                path: 'login',
+                component: Login,
+            },
+            {
+                name: 'User Dashboard',
+                path: 'dashboard',
+                component: Dashboard,
+            },
+            {
+                name: 'Friends',
+                path: 'friends',
+                component: Friends,
+            },
+            {
+                name: 'Matched',
+                path: 'friends/:id',
+                component: Matched,
+            },
+            {
+                name: 'Profile',
+                path: 'profile',
+                component: Profile,
+            }
+        ]
+    },
+
+]
 
 export default new Router({
     mode: 'hash',
     base: process.env.BASE_URL,
-    routes: [
-        {
-            path: '/',
-            redirect: 'home',
-            component: () => import('@/layouts/Layout'),
-            children: [
-                // Components
-                {
-                    name: 'Home',
-                    path: 'home',
-                    component: () => import('@/views/Home'),
-                },
-                {
-                    name: 'Login',
-                    path: 'login',
-                    component: () => import('@/views/Login'),
-                },
-                {
-                    name: 'User Dashboard',
-                    path: 'dashboard',
-                    component: () => import('@/views/Dashboard'),
-                },
-                {
-                    name: 'Friends',
-                    path: 'friends',
-                    component: () => import('@/views/Friends'),
-                },
-                {
-                    name: 'Matched',
-                    path: 'friends/:id',
-                    component: () => import('@/views/Matched'),
-                },
-                {
-                    name: 'Profile',
-                    path: 'profile',
-                    component: () => import('@/views/Profile'),
-                }
-            ]
-        },
-
-    ],
+    routes
 })
